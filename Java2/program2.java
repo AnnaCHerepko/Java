abe62558269c15e51ab8d0ba75b5b01966c89009
AnnaCHerepko <112652363+AnnaCHerepko@users.noreply.github.com>
2023-01-30 18:08:13 +0300

    Add files via upload

---
+++ Java2/program2.java
@@ -0,0 +1,45 @@
+// Реализовать функцию возведения числа а в степень b. a, b ∈ Z. Сводя количество выполняемых действий к минимуму.
+
+public class program2 {
+    String tempB;
+    String tempA;
+    int a;
+    int b;
+
+    public int inputReader(String variable) throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            if (str.contains("a")) {
+                tempA = str.chars()
+                        .filter(c -> !Character.isLetter(c))
+                        .mapToObj(c -> String.valueOf((char) c))
+                        .collect(Collectors.joining())
+                        .trim();
+
+            } else if (str.contains("b")) {
+                tempB = str.chars()
+                        .filter(c -> !Character.isLetter(c))
+                        .mapToObj(c -> String.valueOf((char) c))
+                        .collect(Collectors.joining())
+                        .trim();
+
+            } else {
+                System.out.println("Неверный формат!\n запишите в виде:\nb [число]\na [число]");
+                break;
+            }
+
+        }
+        br.close();
+
+        if (variable == "a"){
+            return a = Integer.parseInt(tempA);
+        }else {
+            return b = Integer.parseInt(tempB);
+        }
+    }
+
+}
+
